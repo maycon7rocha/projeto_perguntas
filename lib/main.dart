97c96fd27d17d66b0c5067336116d52cf3481ac3
Maycon Rocha <maycon7rocha@gmail.com>
2020-10-05 12:16:01 -0300

    aula 52

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,64 @@
-
-
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+
+import './questionario.dart';
+import './resultado.dart';
 
 main() => runApp(PerguntaApp());
 
-class _PerguntaAppState extends State<PerguntaApp>{
-
+class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Tartaruga', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu intrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    },
+  ];
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Coelho', 'Tartaruga', 'Elefante', 'Leão'],
-      },
-      {
-        'texto': 'Qual é o seu intrutor favorito?',
-        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
-      },
-    ];
-
-    List<Widget> respostas = [];
-
-    for (String textoResp in perguntas[_perguntaSelecionada]['respostas']) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
+    // for (String textoResp in respostas) {
+    //   widgets.add(Resposta(textoResp, _responder));
+    // }
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]['texto']),
-            ...respostas,
-            
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder,
+              )
+            : Resultado(),
       ),
     );
   }
 }
 
-
 class PerguntaApp extends StatefulWidget {
-
   @override
   _PerguntaAppState createState() {
     return _PerguntaAppState();
---
+++ lib/resultado.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        'Parabéns!',
+        style: TextStyle(fontSize: 28),
+      ),
+    );
+  }
+}
+
